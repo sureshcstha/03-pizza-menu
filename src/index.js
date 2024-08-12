4cf9399c8a03d862fa462931f0fa62d443a383b0
Suresh Shrestha <sureshcstha@gmail.com>
2024-08-12 13:47:50 -0600

    add more components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const pizzaData = [
   {
@@ -46,30 +46,49 @@ const pizzaData = [
   },
 ];
 
-
 function App() {
   return (
     <div>
-        <h1>Hello React!</h1>
-        <Pizza />
-        <Pizza />
-        <Pizza />
+      <h1>Hello React!</h1>
+      <Header />
+      <Menu />
+      <Footer />
     </div>
-  )
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open.</footer>
+  );
 }
 
 function Pizza() {
   return (
     <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci"/>
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
       <h2>Pizza Spinaci</h2>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
-  )
+  );
 }
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
