3e13afb4aa85efaa3b43ce9a842d54e13e6b8d76
Suresh Shrestha <sureshcstha@gmail.com>
2024-08-12 13:51:37 -0600

    js logic in components

--- src/index.js
+++ src/index.js
@@ -73,6 +73,11 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
   return (
     <footer>{new Date().toLocaleTimeString()} We're currently open.</footer>
   );
