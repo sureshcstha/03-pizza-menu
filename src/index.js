682a319c197325d89b4d6b82fc78f5bcb7bf13ce
Suresh Shrestha <sureshcstha@gmail.com>
2024-08-13 16:32:35 -0600

    conditional rendering with Ternaries

--- src/index.js
+++ src/index.js
@@ -66,18 +66,24 @@ function Header() {
 }
 
 function Menu() {
+
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
   return (
-    <main>
-      <div className="menu">
+      <main className="menu">
         <h2>Our Menu</h2>
 
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
-      </div>
-    </main>
+        {numPizzas > 0 ? (
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        ) : (
+          <p>We're still working on our menu. Please come back later :)</p>
+        )}
+      </main>
   );
 }
 
@@ -102,7 +108,14 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} We're currently open.
+      {isOpen ? (
+        <div className="order">
+          <p>
+            We're open until {closeHour}:00. Come visit us or order online.
+          </p>
+          <button className="btn">Order</button>
+        </div>
+      ) : <p>We are happy to welcome you between {openHour}:00 and {closeHour}:00</p>}
     </footer>
   );
 }
