c142612949ead57dfc06f1089fdce2af7a355c35
Suresh Shrestha <sureshcstha@gmail.com>
2024-08-13 16:44:28 -0600

    extract JSX into a new components

--- src/index.js
+++ src/index.js
@@ -109,15 +109,21 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>
-            We're open until {closeHour}:00. Come visit us or order online.
-          </p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : <p>We are happy to welcome you between {openHour}:00 and {closeHour}:00</p>}
     </footer>
   );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00. Come visit us or order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
