79be08cdbc6d3712eec753e30f59b57d288aaf3a
Suresh Shrestha <sureshcstha@gmail.com>
2024-08-13 16:52:34 -0600

    destructuring props

--- src/index.js
+++ src/index.js
@@ -87,14 +87,14 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
+function Pizza({ pizzaObj }) {
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>${props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>${pizzaObj.price}</span>
       </div>
     </li>
   );
@@ -109,17 +109,17 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <Order closeHour={closeHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : <p>We are happy to welcome you between {openHour}:00 and {closeHour}:00</p>}
     </footer>
   );
 }
 
-function Order(props) {
+function Order({closeHour, openHour}) {
   return (
     <div className="order">
       <p>
-        We're open until {props.closeHour}:00. Come visit us or order online.
+        We're open from {openHour}:00 until {closeHour}:00. Come visit us or order online.
       </p>
       <button className="btn">Order</button>
     </div>
